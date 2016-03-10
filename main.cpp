0d588084ce167bdb79eee816e8500de49fd2a357
Federico Menozzi <federicogmenozzi@gmail.com>
2016-03-10 15:10:34 -0500

    Incorporate SHADEMODE enum

--- main.cpp
+++ main.cpp
@@ -16,13 +16,28 @@
 #include <Sphere.h>
 #include <Util.h>
 
+enum class SHADEMODE {
+    NONE,
+    FLAT,
+    GOURAUD,
+};
+
 constexpr int NX = 512;
 constexpr int NY = 512;
 
 Color* buffer;
 float zbuf[NX][NY];
 
-char shade_mode[20];
+char shade_str[20];
+SHADEMODE shade_mode;
+void assign_shade_mode() {
+    if (strcmp(shade_str, "NONE") == 0)
+        shade_mode = SHADEMODE::NONE;
+    else if (strcmp(shade_str, "FLAT") == 0)
+        shade_mode = SHADEMODE::FLAT;
+    else if (strcmp(shade_str, "GOURAUD") == 0)
+        shade_mode = SHADEMODE::GOURAUD;
+}
 
 void cleanup() {
     delete[] buffer;
@@ -76,7 +91,7 @@ void rasterize(const Triangle& tri, const Light& light, const Material& mat, con
     if (v.dot(tri.n) > 0)
         return;
 
-    // Convert triangles coordinates from world to viewport
+    // Convert triangle coordinates from world to viewport
     auto a_vp = vec4to3(M * Eigen::Vector4f(tri.a[0], tri.a[1], tri.a[2], 1.0f));
     auto b_vp = vec4to3(M * Eigen::Vector4f(tri.b[0], tri.b[1], tri.b[2], 1.0f));
     auto c_vp = vec4to3(M * Eigen::Vector4f(tri.c[0], tri.c[1], tri.c[2], 1.0f));
@@ -106,14 +121,15 @@ void rasterize(const Triangle& tri, const Light& light, const Material& mat, con
 
             if (beta >= 0 && gamma >= 0 && beta + gamma <= 1 && z > zbuf[x][y]) {
                 zbuf[x][y] = z;
-                if (strcmp(shade_mode, "NONE") == 0) {
+                if (shade_mode == SHADEMODE::NONE) {
                     draw(x, y, Color::white());
-                } else if (strcmp(shade_mode, "FLAT") == 0) {
+                } else if (shade_mode == SHADEMODE::FLAT) {
                     draw(x, y, tri.shade(tri.centroid(), tri.n, light, mat));
-                } else if (strcmp(shade_mode, "GOURAUD") == 0) {
-                    Color ac = tri.shade(tri.a, tri.an, light, mat);
-                    Color bc = tri.shade(tri.b, tri.bn, light, mat);
-                    Color cc = tri.shade(tri.c, tri.cn, light, mat);
+                } else if (shade_mode == SHADEMODE::GOURAUD) {
+                    // Vertex colors
+                    auto ac = tri.shade(tri.a, tri.an, light, mat);
+                    auto bc = tri.shade(tri.b, tri.bn, light, mat);
+                    auto cc = tri.shade(tri.c, tri.cn, light, mat);
 
                     draw(x, y, lerp(ac, bc, cc, beta, gamma));
                 }
@@ -125,7 +141,7 @@ void rasterize(const Triangle& tri, const Light& light, const Material& mat, con
 int main(int argc, char* argv[]) {
     // Parse system arguments
     argparser ap = argparser_create(argc, argv, PARSEMODE_LENIENT);
-    argparser_add(&ap, "-s", "--shading", ARGTYPE_STRING, &shade_mode, nullptr);
+    argparser_add(&ap, "-s", "--shading", ARGTYPE_STRING, &shade_str, assign_shade_mode);
     argparser_parse(&ap);
 
     constexpr float l = -0.1f;
@@ -201,10 +217,10 @@ int main(int argc, char* argv[]) {
     #if defined(USE_OPENGL)
         // Write buffer to OpenGL window
         char window_name[50];
-        if      (strcmp(shade_mode, "NONE") == 0)    strcpy(window_name, "Part 1 (Unshaded)");
-        else if (strcmp(shade_mode, "FLAT") == 0)    strcpy(window_name, "Part 2 (Flat Shading)");
-        else if (strcmp(shade_mode, "GOURAUD") == 0) strcpy(window_name, "Part 3 (Gouraud Shading)");
-        else                                         strcpy(window_name, "Part 4 (Phong Shading)");
+        if      (shade_mode == SHADEMODE::NONE)    strcpy(window_name, "Part 1 (Unshaded)");
+        else if (shade_mode == SHADEMODE::FLAT)    strcpy(window_name, "Part 2 (Flat Shading)");
+        else if (shade_mode == SHADEMODE::GOURAUD) strcpy(window_name, "Part 3 (Gouraud Shading)");
+        else                                       strcpy(window_name, "Part 4 (Phong Shading)");
         glutInit(&argc, argv);
         glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
         glutInitWindowSize(NX, NY);
@@ -215,10 +231,10 @@ int main(int argc, char* argv[]) {
     #else
         // Write buffer to image file
         char ppmpath[50];
-        if      (strcmp(shade_mode, "NONE") == 0)    strcpy(ppmpath, "images/part1-unshaded.ppm");
-        else if (strcmp(shade_mode, "FLAT") == 0)    strcpy(ppmpath, "images/part2-flat.ppm");
-        else if (strcmp(shade_mode, "GOURAUD") == 0) strcpy(ppmpath, "images/part3-gouraud.ppm");
-        else                                         strcpy(ppmpath, "images/part4-phong.ppm");
+        if      (shade_mode == SHADEMODE::NONE)    strcpy(ppmpath, "images/part1-unshaded.ppm");
+        else if (shade_mode == SHADEMODE::FLAT)    strcpy(ppmpath, "images/part2-flat.ppm");
+        else if (shade_mode == SHADEMODE::GOURAUD) strcpy(ppmpath, "images/part3-gouraud.ppm");
+        else                                       strcpy(ppmpath, "images/part4-phong.ppm");
         FILE* fp = fopen(ppmpath, "w");
         fprintf(fp, "P3\n");
         fprintf(fp, "%d %d %d\n", NX, NY, 255);
